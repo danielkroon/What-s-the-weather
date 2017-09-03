80c9975e55d2a86c4e640b9d2847774a393df5e4
Daniel Kroon <drg.kroon@gmail.com>
2017-09-03 15:42:57 +0200

    removed some quotations

--- app/scripts/clock.js
+++ app/scripts/clock.js
@@ -1,47 +1,60 @@
 function displayTime() {
-	var currentTime = new Date();
-	var hours = currentTime.getHours();
-	var minutes = currentTime.getMinutes();
-  	var date = currentTime.getDate();
-  	var month = currentTime.getMonth();
-  	var year = currentTime.getFullYear();
+  var currentTime = new Date()
+  var hours = currentTime.getHours()
+  var minutes = currentTime.getMinutes()
+  var date = currentTime.getDate()
+  var month = currentTime.getMonth()
+  var year = currentTime.getFullYear()
 
-	// adding zero's
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	};
+  // adding zero's
+  if (minutes < 10) {
+    minutes = '0' + minutes
+  }
 
-	// create 12-hour format meridiem
-	var meridiem = "AM";
+  // create 12-hour format meridiem
+  var meridiem = 'AM'
 
-	if (hours > 12) {
-		hours = hours - 12;
-		meridiem = "PM"
-	};
+  if (hours > 12) {
+    hours = hours - 12
+    meridiem = 'PM'
+  }
 
-	if (hours === 0) {
-		hours = 12;
-	};
+  if (hours === 0) {
+    hours = 12
+  }
 
-	// create array with months
-	var monthsArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  // create array with months
+  var monthsArray = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'July',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ]
 
-	// grab the clock div
-	var hoursDiv = document.getElementById('clock');
-  	var minutesDiv = document.getElementById('minutes');
+  // grab the clock div
+  var hoursDiv = document.getElementById('clock')
+  var minutesDiv = document.getElementById('minutes')
 
-	// grab the calender div
-	var calenderDiv = document.getElementById('calender');
+  // grab the calender div
+  var calenderDiv = document.getElementById('calender')
 
-	// set the text inside the clock div
-	hoursDiv.innerHTML = hours;
-  	minutesDiv.innerHTML = minutes + " " + meridiem;
+  // set the text inside the clock div
+  hoursDiv.innerHTML = hours
+  minutesDiv.innerHTML = minutes + ' ' + meridiem
 
-	// set the calender inside the calender div
-	calenderDiv.innerHTML = monthsArray[month] + " " + date + " " + year;
+  // set the calender inside the calender div
+  calenderDiv.innerHTML = monthsArray[month] + ' ' + date + ' ' + year
 }
 
-document.onload = displayTime();
+document.onload = displayTime()
 
 // set interval to update clock
-setInterval(displayTime, 1000);
+setInterval(displayTime, 1000)
